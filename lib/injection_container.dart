d0ad6318e4b1728bef5365378802f9c471c37ee9
ESLAM <em2000999@gmail.com>
2024-04-30 13:19:27 +0300

    Added Dependency Injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+import 'features/daily_news/data/data_source/remote/news_api_service.dart';
+import 'features/daily_news/data/repository/article_repository_impl.dart';
+import 'features/daily_news/domain/repository/article_repository.dart';
+import 'features/daily_news/domain/usecases/get_article.dart';
+import 'features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  sl.registerSingleton<Dio>(Dio());
+
+  // Dependencies
+  sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+
+  // UseCases
+  sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
+
+  // Blocs
+  sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
+  
+}
